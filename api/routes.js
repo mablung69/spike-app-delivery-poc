3e0291a370c29e81fd492461f870cf2cd9a6d020
Cristóbal Rojas <mablung69@gmail.com>
2021-04-17 14:54:03 -0400

    added ap router in server

---
+++ api/routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const controller = require('./controller');
+
+const router = express.Router();
+
+// parse application/x-www-form-urlencoded
+router.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
+// parse application/json
+router.use(bodyParser.json({ limit: '50mb' }));
+
+router.get('/historicalData', controller.historicalData);
+router.post('/originDestinyDistance', controller.originDestinationDistanceService);
+
+
+module.exports = router;
