84f3a90278ec17bfd5251ee78956a01b7651d364
Cristóbal Rojas <mablung69@gmail.com>
2021-04-18 18:54:54 -0400

    fix to allow db

--- src/connection/pool.js
+++ src/connection/pool.js
@@ -1,9 +1,11 @@
 const { Pool } = require('pg')
 
 const connectionString = process.env.DATABASE_URL;//'postgresql://user:qwerty@webapp-db:5432/spike-app-delivery-db'
+console.log("connectionString " + connectionString);
 let pool = new Pool({
   connectionString,
 })
+console.log(pool);
 
 module.exports = () => {
     return {  
